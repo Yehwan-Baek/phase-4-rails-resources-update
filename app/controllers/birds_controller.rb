aca93bcd904d535b89d9645f204c5a0f770480f9
Yehwan Baek <baekokok98@gmail.com>
2023-06-16 14:11:33 +1000

    added code

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,14 @@
 class BirdsController < ApplicationController
+  
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
 
   # GET /birds
   def index
@@ -21,6 +31,17 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+
+    # PATCH /birds/:id
+    def update
+      bird = Bird.find_by(id: params[:id])
+      if bird
+        bird.update(bird_params)
+        render json: bird
+      else
+        render json: { error: "Bird not found" }, status: :not_found
+      end
+    end
 
   private
 
